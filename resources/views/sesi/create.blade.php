8d4ba3a7656751599ba284a229b7c05e6e3f9e1f
Daniel Mido <danielmido30@outlook.com>
2025-06-23 12:12:05 +0700

    perbaikan auth dan menyempurnakan layouting sesi

--- resources/views/sesi/create.blade.php
+++ resources/views/sesi/create.blade.php
@@ -1,44 +1,30 @@
 @extends('layout.main')
+
 @section('title', 'Sesi')
 
 @section('content')
-<!--begin::Row-->
 <div class="row">
-  <div class="col-12">
-    <!-- Default box -->
-    <div class="card card-primary card-outline mb-4">
-      <!--begin::Header-->
-      <div class="card-header">
-        <div class="card-title">Tambah Sesi</div>
-      </div>
-      <!--end::Header-->
-      <!--begin::Form-->
-      <form action="{{ route('sesi.store') }}" method="POST">
-        @csrf
-        <!-- @method('POST') -->
-        <form>
-          <!--begin::Body-->
-          <div class="card-body">
-            <div class="cold-md-6 md-3">
-              <label for="nama" class="form-label">Sesi</label>
-              <input type="text" class="form-control" name="nama" value="{{ old('nama') }}">
-              @error('nama')
-              <div class="text-danger"> {{ $message }}</div>
-              @enderror
+    <div class="col-12">
+        <div class="card card-dark card-outline mb-4"> {{-- Changed from card-primary to card-dark --}}
+            <div class="card-header">
+                <div class="card-title">Tambah Sesi</div>
             </div>
-
-          </div>
-          <!--end::Body-->
-          <!--begin::Footer-->
-          <div class="card-footer">
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </div>
-          <!--end::Footer-->
-        </form>
-        <!--end::Form-->
+            <form action="{{ route('sesi.store') }}" method="POST">
+                @csrf
+                <div class="card-body">
+                    <div class="mb-3"> {{-- Changed from cold-md-6 md-3 to mb-3 for consistency --}}
+                        <label for="nama" class="form-label">Nama Sesi</label> {{-- Added 'Nama' for clarity --}}
+                        <input type="text" class="form-control" name="nama" value="{{ old('nama') }}">
+                        @error('nama')
+                        <div class="text-danger"> {{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-dark">Tambah</button> {{-- Changed from btn-primary to btn-dark --}}
+                </div>
+                </form>
+            </div>
     </div>
-  </div>
 </div>
-<!--end::Row-->
-
 @endsection
--- resources/views/sesi/index.blade.php
+++ resources/views/sesi/index.blade.php
@@ -1,68 +1,39 @@
 @extends('layout.main')
+
 @section('title', 'Sesi')
 
 @section('content')
-<!--begin::Row-->
 <div class="row">
   <div class="col-12">
-    <!-- Default box -->
     <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">Sesi Kuliah</h3>
-        <div class="card-tools">
-          <button
-            type="button"
-            class="btn btn-tool"
-            data-lte-toggle="card-collapse"
-            title="Collapse">
-            <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
-            <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
-          </button>
-          <button
-            type="button"
-            class="btn btn-tool"
-            data-lte-toggle="card-remove"
-            title="Remove">
-            <i class="bi bi-x-lg"></i>
-          </button>
-        </div>
-      </div>
       <div class="card-body">
-        <a href="{{ route ('sesi.create')}}" class="btn btn-primary">Tambah Sesi</a>
-        <table class="table table-bordered table-striped">
-          <thead class="table-primary text-center">
+        <a href="{{ route('sesi.create')}}" class="btn btn-dark mb-3"> Tambah </a> {{-- Changed from btn-primary to btn-dark, and reordered --}}
+        <table style="width: 100%;"> {{-- Consistent with Fakultas table styling --}}
+          <thead>
             <tr>
-              <th>Sesi</th>
+              <th style="padding: 8px; text-align: left;">Sesi</th> {{-- Consistent header style --}}
+              <th style="padding: 8px; text-align: left;">Aksi</th> {{-- Added Aksi column for consistency --}}
             </tr>
           </thead>
           <tbody>
             @foreach ($sesi as $item)
             <tr>
-              <td>{{ $item->nama }}</td>
-
+              <td style="padding: 8px;">{{ $item->nama }}</td> {{-- Consistent cell padding --}}
               <td>
-                <a href="{{ route('sesi.show', $item->id) }}" class="btn btn-info">Show</a>
-                <a href="{{ route('sesi.edit', $item->id) }}" class="btn btn-warning">Edit</a>
+                <a href="{{ route('sesi.show', $item->id) }}" class="btn btn-dark mt-2">Show Details</a> {{-- Consistent button style --}}
+                <a href="{{ route('sesi.edit', $item->id) }}" class="btn btn-dark mt-2">Edit</a> {{-- Consistent button style --}}
                 <form action="{{ route('sesi.destroy', $item->id) }}" method="post" class="d-inline">
                   @csrf
                   @method('DELETE')
-                  <button type="submit" class="btn btn-danger show_confirm" data-lte-toggle="tooltip" title="Delete" data-nama="{{$item->nama}}">Delete</button>
+                  <button type="submit" class="btn btn-danger show_confirm mt-2" data-lte-toggle="tooltip" title="Delete" data-nama="{{$item->nama}}">Delete</button>
                 </form>
               </td>
             </tr>
-
             @endforeach
           </tbody>
         </table>
-
       </div>
-      <!-- /.card-body -->
-      <!-- <div class="card-footer">Footer</div> -->
-      <!-- /.card-footer-->
     </div>
-    <!-- /.card -->
   </div>
 </div>
-<!--end::Row-->
-
 @endsection
