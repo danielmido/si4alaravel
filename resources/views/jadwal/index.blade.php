844d5ec28aa0c27d7cc166606bc7f66196842001
Daniel Mido <danielmido30@outlook.com>
2025-06-24 11:45:57 +0700

    perbaikan tampilan crud jadwal (final)

--- resources/views/jadwal/index.blade.php
+++ resources/views/jadwal/index.blade.php
@@ -2,75 +2,48 @@
 @section('title', 'Jadwal')
 
 @section('content')
-<!--begin::Row-->
 <div class="row">
-  <div class="col-12">
-    <!-- Default box -->
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">List Jadwal</h3>
-        <div class="card-tools">
-          <button
-            type="button"
-            class="btn btn-tool"
-            data-lte-toggle="card-collapse"
-            title="Collapse">
-            <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
-            <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
-          </button>
-          <button
-            type="button"
-            class="btn btn-tool"
-            data-lte-toggle="card-remove"
-            title="Remove">
-            <i class="bi bi-x-lg"></i>
-          </button>
+    <div class="col-12">
+        <div class="card">
+            {{-- Removed card-header tools for consistency with Fakultas list page --}}
+            <div class="card-body">
+                <a href="{{ route ('jadwal.create')}}" class="btn btn-dark mb-3"> Tambah </a> {{-- Changed from btn-primary to btn-dark --}}
+                <table style="width: 100%;"> {{-- Consistent with Fakultas table styling --}}
+                    <thead>
+                        <tr>
+                            <th style="padding: 8px; text-align: left;">Tahun Akademik</th>
+                            <th style="padding: 8px; text-align: left;">Kode Semester</th>
+                            <th style="padding: 8px; text-align: left;">Kelas</th>
+                            <th style="padding: 8px; text-align: left;">Matakuliah</th>
+                            <th style="padding: 8px; text-align: left;">Dosen</th>
+                            <th style="padding: 8px; text-align: left;">Sesi</th>
+                            <th style="padding: 8px; text-align: left;">Aksi</th> {{-- Added Aksi column for consistency --}}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($jadwal as $item)
+                        <tr>
+                            <td style="padding: 8px;">{{ $item->tahun_akademik }}</td>
+                            <td style="padding: 8px;">{{ $item->kode_smt }}</td>
+                            <td style="padding: 8px;">{{ $item->kelas }}</td>
+                            <td style="padding: 8px;">{{ $item->matakuliah->nama }}</td>
+                            <td style="padding: 8px;">{{ $item->users->name }}</td>
+                            <td style="padding: 8px;">{{ $item->sesi->nama }}</td>
+                            <td>
+                                <a href="{{ route('jadwal.show', $item->id) }}" class="btn btn-dark mt-2">Show Details</a> {{-- Consistent button style --}}
+                                <a href="{{ route('jadwal.edit', $item->id) }}" class="btn btn-dark mt-2">Edit</a> {{-- Consistent button style --}}
+                                <form action="{{ route('jadwal.destroy', $item->id) }}" method="post" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger show_confirm mt-2" data-lte-toggle="tooltip" title="Delete" data-nama="Jadwal {{ $item->tahun_akademik }} - {{ $item->matakuliah->nama }}">Delete</button> {{-- Adjusted data-nama for clarity --}}
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            </div>
         </div>
-      </div>
-      <div class="card-body">
-        <a href="{{ route ('jadwal.create')}}" class="btn btn-primary mb-3">Tambah Jadwal</a>
-        <table class="table table-bordered table-striped align-middle">
-          <thead class="table-primary text-center">
-            <tr>
-              <th>Tahun Akademik</th>
-              <th>Kode Semester</th>
-              <th>Kelas</th>
-              <th>Matakuliah</th>
-              <th>Dosen</th>
-              <th>Sesi</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($jadwal as $item)
-            <tr>
-              <td>{{ $item->tahun_akademik }}</td>
-              <td>{{ $item->kode_smt }}</td>
-              <td>{{ $item->kelas }}</td>
-              <td>{{ $item->matakuliah->nama }}</td>
-              <td>{{ $item->users->name }}</td>
-              <td>{{ $item->sesi->nama }}</td>
-              <td>
-                <a href="{{ route('jadwal.show', $item->id) }}" class="btn btn-info">Show</a>
-                <a href="{{ route('jadwal.edit', $item->id) }}" class="btn btn-warning">Edit</a>
-                <form action="{{ route('jadwal.destroy', $item->id) }}" method="post" class="d-inline">
-                  @csrf
-                  @method('DELETE')
-                  <button type="submit" class="btn btn-danger show_confirm" data-lte-toggle="tooltip" title="Delete" data-nama="{{$item->nama}}">Delete</button>
-                </form>
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
-
-      </div>
-      <!-- /.card-body -->
-      <!-- <div class="card-footer">Footer</div> -->
-      <!-- /.card-footer-->
-    </div>
-    <!-- /.card -->
-  </div>
 </div>
-<!--end::Row-->
-
 @endsection
--- resources/views/jadwal/show.blade.php
+++ resources/views/jadwal/show.blade.php
@@ -2,64 +2,41 @@
 @section('title', 'Jadwal')
 
 @section('content')
-<!--begin::Row-->
 <div class="row">
-  <div class="col-12">
-    <!-- Default box -->
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">List Jadwal</h3>
-        <div class="card-tools">
-          <button
-            type="button"
-            class="btn btn-tool"
-            data-lte-toggle="card-collapse"
-            title="Collapse">
-            <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
-            <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
-          </button>
-          <button
-            type="button"
-            class="btn btn-tool"
-            data-lte-toggle="card-remove"
-            title="Remove">
-            <i class="bi bi-x-lg"></i>
-          </button>
+    <div class="col-12">
+        <div class="card">
+            {{-- Removed card-header tools for consistency with Fakultas show page --}}
+            <div class="card-body">
+                <table class="table"> {{-- Simpler table as in Fakultas show page --}}
+                    <tbody>
+                        <tr>
+                            <th>Tahun Akademik</th>
+                            <td>{{ $jadwal->tahun_akademik}}</td>
+                        </tr>
+                        <tr>
+                            <th>Kode Semester</th>
+                            <td>{{ $jadwal->kode_smt}}</td>
+                        </tr>
+                        <tr>
+                            <th>Kelas</th>
+                            <td>{{ $jadwal->kelas}}</td>
+                        </tr>
+                        <tr>
+                            <th>Matakuliah</th>
+                            <td>{{ $jadwal->matakuliah->nama}}</td>
+                        </tr>
+                        <tr>
+                            <th>Dosen</th>
+                            <td>{{ $jadwal->users->name}}</td>
+                        </tr>
+                        <tr>
+                            <th>Sesi</th>
+                            <td>{{ $jadwal->sesi->nama}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            </div>
         </div>
-      </div>
-      <div class="card-body">
-        <div class="table-responsive">
-          <a href="{{ route ('jadwal.index')}}" class="btn btn-primary mb-3">Kembali</a>
-          <table class="table table-bordered table-striped">
-            <tr>
-              <th>Tahun Akademik</th>
-              <td>{{ $jadwal-> tahun_akademik}}</td>
-            </tr>
-            <tr>
-              <th>kode Semester</th>
-              <td>{{ $jadwal-> kode_smt}}</td>
-            </tr>
-            <tr>
-              <th>Matakuliah</th>
-              <td>{{ $jadwal->matakuliah->nama}}</td>
-            </tr>
-            <tr>
-              <th>Dosen</th>
-              <td>{{ $jadwal->users->name}}</td>
-            </tr>
-            <tr>
-              <th>Sesi</th>
-              <td>{{ $jadwal->sesi->nama}}</td>
-            </tr>
-          </table>
-        </div>
-      </div>
-      <!-- /.card-body -->
-      <!-- <div class="card-footer">Footer</div> -->
-      <!-- /.card-footer-->
-    </div>
-    <!-- /.card -->
-  </div>
 </div>
 @endsection
-<!--end::Row-->
