bc723dc38257c2a6b522651ebadc610989c9f879
Daniel Mido <danielmido30@outlook.com>
2025-06-24 11:43:00 +0700

    penyesuaian tampilan page mata kuliah

--- resources/views/matakuliah/index.blade.php
+++ resources/views/matakuliah/index.blade.php
@@ -1,70 +1,51 @@
 @extends('layout.main')
-@section('title', 'MataKuliah')
 
+<!-- Sections Title-->
+@section('title', 'Mata Kuliah')
+
+<!-- Section isian -->
 @section('content')
 <!--begin::Row-->
 <div class="row">
-  <div class="col-12">
-    <!-- Default box -->
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">List Matakuliah</h3>
-        <div class="card-tools">
-          <button
-            type="button"
-            class="btn btn-tool"
-            data-lte-toggle="card-collapse"
-            title="Collapse">
-            <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
-            <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
-          </button>
-          <button
-            type="button"
-            class="btn btn-tool"
-            data-lte-toggle="card-remove"
-            title="Remove">
-            <i class="bi bi-x-lg"></i>
-          </button>
+    <div class="col-12">
+        <!-- Default box -->
+        <div class="card">
+            {{-- Removed card-header tools for consistency with Fakultas list page --}}
+            <div class="card-body">
+                <a href="{{ route ('matakuliah.create')}}" class="btn btn-dark mb-3"> Tambah </a> {{-- Changed from btn-primary to btn-dark --}}
+                <table style="width: 100%;"> {{-- Consistent with Fakultas table styling --}}
+                    <thead>
+                        <tr>
+                            <th style="padding: 8px; text-align: left;">Kode MK</th>
+                            <th style="padding: 8px; text-align: left;">Nama</th>
+                            <th style="padding: 8px; text-align: left;">Prodi</th>
+                            <th style="padding: 8px; text-align: left;">Aksi</th> {{-- Added Aksi column for consistency --}}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($matakuliah as $item)
+                        <tr>
+                            <td style="padding: 8px;">{{ $item->kode_mk }}</td>
+                            <td style="padding: 8px;">{{ $item->nama }}</td>
+                            <td style="padding: 8px;">{{ $item->prodi->nama }}</td>
+                            <td>
+                                <a href="{{ route('matakuliah.show', $item->id) }}" class="btn btn-dark mt-2">Show Details</a> {{-- Consistent button style --}}
+                                <a href="{{ route('matakuliah.edit', $item->id) }}" class="btn btn-dark mt-2">Edit</a> {{-- Consistent button style --}}
+                                <form action="{{ route('matakuliah.destroy', $item->id) }}" method="post" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger show_confirm mt-2" data-lte-toggle="tooltip" title="Delete" data-nama="{{$item->nama}}">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
         </div>
-      </div>
-      <div class="card-body">
-        <a href="{{ route ('matakuliah.create')}}" class="btn btn-primary">Tambah Matakuliah</a>
-        <table class="table table-bordered table-striped">
-          <thead>
-            <tr>
-              <th>Kode MK</th>
-              <th>Nama</th>
-              <th>Prodi</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($matakuliah as $item)
-            <tr>
-              <td>{{ $item->kode_mk }}</td>
-              <td>{{ $item->nama }}</td>
-              <td>{{ $item->prodi->nama }}</td>
-              <td>
-                <a href="{{ route('matakuliah.show', $item->id) }}" class="btn btn-info">Show</a>
-                <a href="{{ route('matakuliah.edit', $item->id) }}" class="btn btn-warning">Edit</a>
-                <form action="{{ route('matakuliah.destroy', $item->id) }}" method="post" class="d-inline">
-                  @csrf
-                  @method('DELETE')
-                  <button type="submit" class="btn btn-danger show_confirm" data-lte-toggle="tooltip" title="Delete" data-nama="{{$item->nama}}">Delete</button>
-                </form>
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
-
-      </div>
-      <!-- /.card-body -->
-      <!-- <div class="card-footer">Footer</div> -->
-      <!-- /.card-footer-->
+        <!-- /.card -->
     </div>
-    <!-- /.card -->
-  </div>
 </div>
 <!--end::Row-->
-
 @endsection
